fb9ad14edd9fd1d6321b6e5af21ee9d77d0b2dea
Milstein <milsonmun@yahoo.com>
2016-10-18 18:54:58 -0600

    updated

--- src/test/java/gpms/selenium/TestMaster.java
+++ src/test/java/gpms/selenium/TestMaster.java
@@ -4,25 +4,27 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-		// SuccessfullProposalSubmition_IRB.class,
-		// SuccessfullProposalSubmition_NoIRB.class,
-		// SuccessfullProposalSubmitionIRB_twoFaculty.class,
-		BusinessManagerApprovalFail.class,
-		BusinessManagerAttatchmentFail.class, ChairApprovalFail.class,
+@Suite.SuiteClasses({ BusinessManagerAttatchmentFail.class,
+		BusinessManagerApprovalFail.class, ChairApprovalFail.class,
 		ChairAttatchmentFail.class, CoPIaddsSeniorPersonal.class,
 		CoPIattemptsProposalDeletion.class, CoPIattemptsToDeleteCoPI.class,
 		CoPIproposalSubmissionFail.class, DeanApprovalFail.class,
 		DeanAttatchmentFail.class, DeanDeletesAttatchmentFail.class,
-		DeanFileUploadFail.class, PIaddsCoPI.class,
-		PIaddsFileAttatchment.class, PIdeletesAttatchment.class,
-		PIdeletesCoPI.class, PIFileAttatchmentUpload.class,
-		PIuploadsFileAttatchment.class, ProposalAddFailure.class,
-		ResearchAdminApprovalFail.class, ResearchAdminProposalWithdrawn.class,
+		DeanFileUploadFail.class, DelegationByChair.class, PIaddsCoPI.class,
+		PIaddsFileAttatchment.class, PIbuttonPermissions.class,
+		PIdeletesAttatchment.class, PIdeletesCoPI.class,
+		PIFileAttatchmentUpload.class, PIuploadsFileAttatchment.class,
+		ProposalAddFailure.class, ResearchAdminApprovalFail.class,
+		ResearchAdminProposalWithdrawn.class,
 		ResearchDirectorApprovalFail.class,
+		ResearchDirectorProposalDeletionFail.class,
+		ResearchDirectorProposalWithdrawn.class,
 		SeniorPersonalProposalDeleteFail.class,
-		SeniorPersonalProposalSaveFail.class, TenuredChemFacultyProposal.class,
-		DelegationByChair.class })
+		SeniorPersonalProposalSaveFail.class,
+		SuccessfullProposalSubmition_IRB.class,
+		SuccessfullProposalSubmition_NoIRB.class,
+		SuccessfullProposalSubmitionIRB_twoFaculty.class,
+		TenuredChemFacultyProposal.class })
 public class TestMaster {
 	public TestMaster() {
 	}
